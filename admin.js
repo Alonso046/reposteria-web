b9aec21f0f5b86b4f3d44e7858a2fd2491896276
Alonso046 <alonso.vera@alumnos.uv.cl>
2026-09-11 17:01:55 -0300

    Actualizando URL del render  a la nube

--- admin.js
+++ admin.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://reposteria-api.onrender.com";
+const API_BASE_URL = "https://reposteria-api-tgz9.onrender.com";
 let productToDeleteId = null; 
 
 document.addEventListener("DOMContentLoaded", () => {
